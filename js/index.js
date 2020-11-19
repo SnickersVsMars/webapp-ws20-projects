494337d30e6b85248cf2ce780b6d696d690b71f2
Michael Lamprecht <lammia07@gmail.com>
2020-11-19 20:37:21 +0100

    Bugfixing

--- js/index.js
+++ js/index.js
@@ -3,38 +3,57 @@ var template = document.getElementById("card-template");
 
 
 $.ajax({
-    url:"data/project_sample_data.json",
+    url: datafilepath,
     dataType:"json",
     type:"get",
     cache: false,
     success: function (items){
         $(items.projects).each(function(i, project){
             var card = template.content.cloneNode(true);
-            card.querySelector(".project-number").innerHTML = project.number;
-            card.querySelector(".project-manager").innerHTML = project.manager;
-            card.querySelector(".project-employees").innerHTML = project.employees.length;
-            card.querySelector(".project-label").innerHTML = project.label;
-            card.querySelector(".project-milestone").innerHTML = lastMilestone(project.milestones);
-            card.querySelector(".project-customer").innerHTML = project.customer;
+            card.querySelector(".project-number").innerHTML = validate(project.number);
+            card.querySelector(".project-manager").innerHTML = validate(project.manager);
+
+            card.querySelector(".project-label").innerHTML = validate(project.label);
+            card.querySelector(".project-milestone").innerHTML = validate(lastMilestone(project.milestones));
+            card.querySelector(".project-customer").innerHTML = validate(project.customer);
+
+            if (project.employees != null && project.employees != 'undefined' && project.employees.length > 0)
+                card.querySelector(".project-employees").innerHTML = project.employees.length;
+
             overview.appendChild(card);
             var cards = document.getElementsByClassName('card');
             cards[cards.length-1].addEventListener("click",()=>showDetail(project.id));
+
+
+
         });
         document.getElementById("busy-indicator").hidden = true;
     }
 });
+
+
 
 function showDetail(id){
     location.href = "detailsite.html?id=" + id;
 };
 
 function lastMilestone(milestones){
+    if (milestones === null || milestones === undefined || milestones.length < 1)
+        return;
+
     var lastMilestone = null;
     var today = new Date();
     for (var i= 0; i < milestones.length; i++) {
+        if(milestones[i].date === null || milestones[i].date === undefined)
+            continue;
+
         var milestoneDate = new Date(milestones[i].date)
         if(today <= milestoneDate && (lastMilestone === null || milestoneDate < lastMilestone))
             lastMilestone = milestoneDate;
     }
+
+    if(lastMilestone == null)
+        return validate(null);
+
     return lastMilestone.toLocaleDateString();
 }
---
+++ js/main.js
@@ -0,0 +1,8 @@
+var datafilepath="data/project_sample_data.json";
+
+function validate(value) {
+    if(value === null || value === undefined)
+        return "&ndash;";
+
+    return value;
+}

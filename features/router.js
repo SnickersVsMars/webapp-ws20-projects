39938dc37d7856bc1bc8f132e7e84a8e69fdfa4e
Michael Lamprecht <michael.lamprecht@edu.fh-joanneum.at>
2020-12-01 16:20:52 +0100

    static error pages (#4)
    * static error pages
    
    * remove ajax from error pages
    
    * Update features/errors/404.html
    
    Co-authored-by: Marian Korošec <marian.korosec@gmx.at>
    
    * Update features/errors/500.html
    
    Co-authored-by: Marian Korošec <marian.korosec@gmx.at>
    
    Co-authored-by: Marian Korošec <marian.korosec@gmx.at>

--- features/router.js
+++ features/router.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const path = require('path');
 const router = express.Router();
 
 router.use('/', require('./projects/projectRouter'));
 router.use('/', require('./test/testRouter'));
+
+router.get('/404', (req, res) => {
+    res.sendFile(path.join(__dirname, '/errors/404.html'));
+});
+
+router.get('/500', (req, res) => {
+    res.sendFile(path.join(__dirname, '/errors/500.html'));
+});
 
 router.get('/', (req, res) => {
     res.redirect('/projects');

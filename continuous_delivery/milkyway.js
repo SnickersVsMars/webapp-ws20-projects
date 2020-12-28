cc60a65956996f532b8c1e11aa959702a10984c1
Christian Sitzwohl <christian.sitzwohl@edu.fh-joanneum.at>
2020-12-28 18:20:12 +0100

    update

--- continuous_delivery/milkyway.js
+++ continuous_delivery/milkyway.js
@@ -4,9 +4,9 @@ const Url = require('url-parse');
  
 var server = http.createServer(function (req, res) {
   console.log("Server listening on port 4000");
-  var url = new Url(req.url);
-  req.forward = { target: url.hostname+':3000' };
-  forward(req, res);
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('Hello World!');
+  res.end();
 });
  
 server.listen(4000, () =>

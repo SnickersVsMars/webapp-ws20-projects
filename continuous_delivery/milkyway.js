d0e8663ce5ea1a43cdb7bf9d1fede8d8148b3e7c
Christian Sitzwohl <christian.sitzwohl@edu.fh-joanneum.at>
2020-12-28 18:13:43 +0100

    update

--- continuous_delivery/milkyway.js
+++ continuous_delivery/milkyway.js
@@ -1,12 +1,13 @@
-const http = require('http')
-const forward = require('http-forward')
+const http = require('http');
+const forward = require('http-forward');
+const Url = require('url-parse');
  
 var server = http.createServer(function (req, res) {
-  var parse = require('url-parse')
-  , url = parse(req.url, true);
-  // Define proxy config params
-  req.forward = { target: hostname+':3000' }
-  forward(req, res)
-})
+  var url = new Url(req.url);
+  req.forward = { target: url.hostname+':3000' };
+  forward(req, res);
+});
  
-server.listen(4000)
+server.listen(4000, () =>
+    console.log("Server listening on port 4000")
+);

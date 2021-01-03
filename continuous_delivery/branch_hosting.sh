f3d4e54bf68866689261438760490327ea0b04bc
Christian Sitzwohl <christian.sitzwohl@edu.fh-joanneum.at>
2021-01-03 12:50:09 +0100

    hosting script

--- continuous_delivery/branch_hosting.sh
+++ continuous_delivery/branch_hosting.sh
@@ -10,8 +10,10 @@ then
 	port=$(mysql -D$MYDB -u$MYUSER -p$MYPASS -se "SELECT MAX(port)+1 FROM HostingTable")
 	if  [ -z "$port" -o "$port" = "NULL" ]
 	then
+		echo 'default port'
 		port=3001
 	fi	
+	echo 'insert port into routing table'
 	$(mysql -D$MYDB -u$MYUSER -p$MYPASS -se "INSERT INTO HostingTable VALUES('${route}',$port,now())")	
 fi
 echo $port

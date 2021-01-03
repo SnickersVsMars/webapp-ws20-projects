f556f1fc81bd6e8b27c34871a9adaa610d3e1b06
Christian Sitzwohl <christian.sitzwohl@edu.fh-joanneum.at>
2021-01-03 12:31:23 +0100

    hosting script

--- continuous_delivery/branch_hosting.sh
+++ continuous_delivery/branch_hosting.sh
@@ -21,7 +21,7 @@ cd "/var/www/html/projects/staging/webapp-ws20-projects/${branch}"
 
 if [ ! -d "/var/www/html/projects/staging/webapp-ws20-projects/${branch}/webapp/.git" ]
 then
-    git clone 'https://github.com/SnickersVsMars/webapp-ws20-projects.git' '/var/www/html/projects/staging/webapp-ws20-projects/${branch}/webapp/'
+    git clone "https://github.com/SnickersVsMars/webapp-ws20-projects.git" "/var/www/html/projects/staging/webapp-ws20-projects/${branch}/webapp/"
 else
     cd "/var/www/html/projects/staging/webapp-ws20-projects/${branch}/webapp/"
     git pull 'https://github.com/SnickersVsMars/webapp-ws20-projects.git'

03860d00588e590ad222e67a13a731126b345624
Christian Sitzwohl <christian.sitzwohl@edu.fh-joanneum.at>
2021-01-03 12:35:08 +0100

    hosting script

--- continuous_delivery/branch_hosting.sh
+++ continuous_delivery/branch_hosting.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 MYUSER="user" MYPASS="IleXTrUegYPhopECTAL" MYDB="hosting" route="/${1}" branch=$1
-echo route
-echo branch
+echo $route
+echo $branch
 
 port=$(mysql -D$MYDB -u$MYUSER -p$MYPASS -se "SELECT port FROM HostingTable WHERE route ='${route}'")
 
@@ -14,22 +14,26 @@ then
 	fi	
 	$(mysql -D$MYDB -u$MYUSER -p$MYPASS -se "INSERT INTO HostingTable VALUES('${route}',$port,now())")	
 fi
-
+echo $port
 
 mkdir -p "/var/www/html/projects/staging/webapp-ws20-projects/${branch}"
 cd "/var/www/html/projects/staging/webapp-ws20-projects/${branch}"
 
 if [ ! -d "/var/www/html/projects/staging/webapp-ws20-projects/${branch}/webapp/.git" ]
 then
+	echo 'clone'
     git clone "https://github.com/SnickersVsMars/webapp-ws20-projects.git" "/var/www/html/projects/staging/webapp-ws20-projects/${branch}/webapp/"
 else
+	echo 'pull'
     cd "/var/www/html/projects/staging/webapp-ws20-projects/${branch}/webapp/"
     git pull 'https://github.com/SnickersVsMars/webapp-ws20-projects.git'
 fi
 
+echo 'switch branch'
 cd "/var/www/html/projects/staging/webapp-ws20-projects/${branch}/webapp/"
 git checkout ci
 git pull
 
+echo 'npm start'
 npm install
 PORT=$port npm start 

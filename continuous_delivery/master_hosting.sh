cbc269b7b07dd23e1a3f3e19c4adf39fdcd409a2
Christian Sitzwohl <sitcha07@gmail.com>
2021-01-08 20:21:30 +0100

    Update master_hosting.sh

--- continuous_delivery/master_hosting.sh
+++ continuous_delivery/master_hosting.sh
@@ -1,1 +1,12 @@
+#!/bin/sh
+MASTER_DIR="/var/www/html/projects/webapp-ws20-projects/"
+GIT_PATH="https://github.com/SnickersVsMars/webapp-ws20-projects.git"
 
+cd "${MASTER_DIR}/${branch}/webapp/"
+git checkout master
+git pull
+
+echo 'npm start'
+npm install
+pm2 delete webapp
+pm2 start npm --name webapp -- start

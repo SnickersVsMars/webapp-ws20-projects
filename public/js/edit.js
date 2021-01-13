f1dfe3e9f1d0d2d4e8fbeaab1767ba11fab04faf
martin-gk <>
2021-01-13 21:07:08 +0100

    project data dynamically added on edit.html with edit.js

--- public/js/edit.js
+++ public/js/edit.js
@@ -1,5 +1,5 @@
 const split = window.location.href.split('/');
-const id = split[split.length - 2];
+const id = split[split.length - 3];
 
 HttpService.get('projects/' + id)
     .then((project) => {
@@ -12,22 +12,20 @@ HttpService.get('projects/' + id)
         }
     });
 
-document.getElementById('getme').innerText = 'hi ';
+function populateData(project) {
+    document.getElementById('input-label').value = validate(project.label);
+    document.getElementById('input-number').value = validate(project.number);
 
-/*function populateData(project) {
-    document.getElementById('label').placeholder = 'testtesttest';
-    document.getElementById('number').placeholder = validate(project.number);
+    setDescription(document.getElementById('input-description'), project.description);
 
-    setDescription(document.getElementById('description'), project.description);
-
-    document.getElementById('manager').placeholder = validate(project.manager);
-    document.getElementById('customer').placeholder = validate(project.customer);
-    document.getElementById('costCenter').placeholder = validate(
+    document.getElementById('input-manager').value = validate(project.manager);
+    document.getElementById('input-customer').value = validate(project.customer);
+    document.getElementById('input-costcenter').value = validate(
         project.costCenter
     );
     document.getElementById(
         'breadcrumb'
-    ).placeholder = `PROJEKT ${project.number}`;
+    ).innerText = `PROJEKT ${project.number}`;
     document
         .getElementById('breadcrumb')
         .setAttribute('href', '/projects/' + project.id);
@@ -37,23 +35,51 @@ document.getElementById('getme').innerText = 'hi ';
 
     document.getElementById('busy-indicator').hidden = true;
 }
+function addEmployeeField(value) {
+    var employeeContainer = document.getElementById('employee-container');
+
+    var newTextfield = document.createElement('input');
+    newTextfield.setAttribute('type', 'text');
+    newTextfield.setAttribute('class', 'form-control mb-2 input-employee');
+    newTextfield.setAttribute('name', 'employees[][name]');
+    newTextfield.setAttribute('required', true);
+    newTextfield.setAttribute('maxlength', 100);
+    newTextfield.setAttribute('placeholder', 'Name des Mitarbeiters');
+    newTextfield.setAttribute('value', value);
+    if (value === '' || undefined) {
+        newTextfield.removeAttribute('value');
+    }
+
+    var removeButton = createRemoveButton('Mitarbeiter entfernen', (event) => {
+        $(event.target).closest('.added-employee').remove();
+    });
+
+    var container = document.createElement('div');
+    container.classList = 'added-employee';
+    container.appendChild(removeButton);
+    container.appendChild(newTextfield);
+
+    employeeContainer.appendChild(container);
+}
+
 
 function fillEmployess(employees) {
     if (employees === null || employees === undefined || employees.length < 1)
         return;
 
-    let ul = document.getElementById('employees');
+   // let ul = document.getElementById('employees');
 
     for (let i = 0; i < employees.length; i++) {
+        addEmployeeField(employees[i].name);
         // Create the list item:
-        let item = document.createElement('li');
+       // let item = document.createElement('li');
         // item.className="list-group-item"
 
         // Set its contents:
-        item.innerText = employees[i].name;
+       // item.innerText = employees[i].name;
 
         // Add it to the list:
-        ul.appendChild(item);
+        //ul.appendChild(item);
     }
 }
 
@@ -91,4 +117,22 @@ function setDescription(element, description) {
     } else {
         element.innerText = description;
     }
-}*/
+}
+
+function createRemoveButton(title, onclick) {
+    let icon = document.createElement('i');
+    icon.classList = 'material-icons';
+    icon.innerText = 'remove';
+
+    let buttonRemove = document.createElement('button');
+    buttonRemove.setAttribute('type', 'button');
+    buttonRemove.classList = 'btn btn-danger mt-1 mb-1 remove-button';
+    buttonRemove.setAttribute('title', title);
+
+    buttonRemove.appendChild(icon);
+
+    buttonRemove.onclick = onclick;
+
+    return buttonRemove;
+}
+

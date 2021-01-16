3bb2e5db3690885aac108d6feff05b589442ebe1
martin-gk <>
2021-01-16 17:33:02 +0100

    finished client-side

--- public/js/form.js
+++ public/js/form.js
@@ -31,61 +31,64 @@
         false
     );
 })();
+const handleSubmitResult = (res) => {
+    if (typeof res === 'number') {
+        location.href = '/projects/' + res;
+        return;
+    }
+    for (const [key, value] of Object.entries(res.responseJSON.errors)) {
+        console.log(`${key}: ${value}`);
+        if (key === 'milestones') {
+            $('#milestone-error').append(
+                '<div class="alert alert-danger" role="alert">' +
+                    value +
+                    '</div>'
+            );
+            return;
+        }
+
+        let element;
+
+        if (key.includes('milestones[')) {
+            let re = /\[\d+\]/;
+            let match = key.match(re)[0];
+            let keyIndex = parseInt(match.substring(1, match.length - 1));
+
+            re = /\]\..*/;
+            match = key.match(re)[0];
+            let name =
+                '[name="milestones[][' +
+                match.substring(2, match.length) +
+                ']"]';
+
+            $(name).each((index, item) => {
+                if (index === keyIndex) {
+                    element = $(item);
+                }
+            });
+        } else {
+            element = $('[name ="' + key + '"]');
+        }
+
+        element.addClass('is-invalid');
+        let feedback = element.siblings('.invalid-feedback');
+        if (feedback.length >= 1) feedback.text(value);
+        else
+            element
+                .parent()
+                .append('<div class="invalid-feedback">' + value + '</div>');
+    }
+};
 
 const handleFormSubmit = (form) => {
     data = $(form).serializeJSON({
         skipFalsyValuesForTypes: ['string'],
     });
-    HttpService.post('projects', data).always((res) => {
-        if (typeof res === 'number') {
-            location.href = '/projects/' + res;
-            return;
-        }
-
-        for (const [key, value] of Object.entries(res.responseJSON.errors)) {
-            console.log(`${key}: ${value}`);
-            if (key === 'milestones') {
-                $('#milestone-error').append(
-                    '<div class="alert alert-danger" role="alert">' +
-                        value +
-                        '</div>'
-                );
-
-                return;
-            }
-
-            let element;
-
-            if (key.includes('milestones[')) {
-                let re = /\[\d+\]/;
-                let match = key.match(re)[0];
-                let keyIndex = parseInt(match.substring(1, match.length - 1));
-
-                re = /\]\..*/;
-                match = key.match(re)[0];
-                let name =
-                    '[name="milestones[][' +
-                    match.substring(2, match.length) +
-                    ']"]';
-
-                $(name).each((index, item) => {
-                    if (index === keyIndex) {
-                        element = $(item);
-                    }
-                });
-            } else {
-                element = $('[name ="' + key + '"]');
-            }
-
-            element.addClass('is-invalid');
-            let feedback = element.siblings('.invalid-feedback');
-            if (feedback.length >= 1) feedback.text(value);
-            else
-                element
-                    .parent()
-                    .append(
-                        '<div class="invalid-feedback">' + value + '</div>'
-                    );
-        }
-    });
+    if (data.hasOwnProperty('projectId')) {
+        HttpService.put('projects/' + data.projectId, data).always(
+            handleSubmitResult
+        );
+    } else {
+        HttpService.post('projects', data).always(handleSubmitResult);
+    }
 };

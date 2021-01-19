e0ca4beee2462911891e1282d3b9c471ff67c64d
Michael Lamprecht <lammia07@gmail.com>
2021-01-19 20:10:48 +0100

    fix date validation

--- features/projects/projectValidationService.js
+++ features/projects/projectValidationService.js
@@ -57,8 +57,10 @@ let validationArray = [
         .trim()
         .notEmpty()
         .withMessage('Bitte Datum eintragen')
-        .matches(/\d{4}-\d{2}-\d{2}/)
-        .withMessage('Format nicht korrekt, bitte JJJJ-MM-TT verwenden'),
+        .matches(/^(199\d|20\d{2})-(0\d|1(0|1|2))-(0\d|1\d|2\d|3(0|1|))$/)
+        .withMessage(
+            'Format nicht korrekt, bitte Format JJJJ-MM-TT verwenden. Frühestes eingabe Datum 1990-01-01.'
+        ),
 
     body('milestones').custom((value) => {
         let startDate;
